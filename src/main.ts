3fd4b15c09c1ae7a435d8b6c66d0574960765fe4
Juan Yepes <juanferhiguita65@gmail.com>
2024-09-12 12:54:03 -0500

    Fix: Routing status 400

--- src/main.ts
+++ src/main.ts
@@ -3,3 +3,8 @@ import App from './App.vue'
 import router from './router'
 
 createApp(App).use(router).mount('#app')
+
+
+//subimos al repo el proyecto, para netlify configuramos el netlify.toml y el _redirects: para que nos redireccione a la ruta /index.html
+
+//Para host normal se suben los archivos dist al host
